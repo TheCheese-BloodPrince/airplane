f21037dfa8f3d1f7bf913db55724cc0b5a39bc04
Aarav Karasi <karasiaarav@gmail.com>
2021-04-19 14:41:44 -0700

    Added AFK/Info commands

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from keep_alive import keep_alive
 from discord.ext.commands import has_permissions
+from replit import db
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = commands.Bot(command_prefix='+')
@@ -30,7 +31,30 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 async def ban(ctx, member : discord.Member, *, reason=None):
   await member.ban(reason=reason)
 
-#Commands
+#+info
+@bot.command(name='info', description='Information about airplane.')
+async def info(ctx):
+  ctx.send("airplane is an open-source Discord moderation/economy bot made by The Cheese-Blood Prince#0505.")
+
+#+afk
+@bot.command(name='afk', description='Sets the user to afk.')
+async def afk(ctx, reason):
+  if ctx.author.nick == None:
+    await ctx.author.edit(nick=("["+reason+"]"+ctx.author.name))
+    db[str(ctx.author.id)+"_nick"] = ctx.author.name
+  else:
+    previousNick = ctx.author.nick
+    await ctx.author.edit(nick=("["+reason+"]"+previousNick))
+    db[str(ctx.author.id)+"_nick"] = ctx.author.nick
+  await ctx.send("**"+ctx.author.name+"** is AFK: " + reason)
+
+#+back
+@bot.command(name='back', description="Removes the user's afk.")
+async def back(ctx):
+  await ctx.author.edit(nick=db[str(ctx.author.id)+"_nick"])
+  await ctx.send("**"+ctx.author.name+"** is back! *attempts to smile*")
+
+
 #Running the Bot
 keep_alive()
 bot.run(TOKEN)
