bf3738e8982082939c568810757591841fe5cd8d
Aarav Karasi <karasiaarav@gmail.com>
2021-04-19 07:07:58 -0700

    Added basic kick command
    Cause replit not saving :(

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import json
 from keep_alive import keep_alive
+from discord_slash import SlashCommand, SlashContext
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-bot = commands.Bot(command_prefix=';')
+bot = commands.Bot(command_prefix='e')
 
 @bot.event
 async def on_ready():
   print("airplane has started up; Can now execute commands.")
+
+
+
+
+#Commands
+
+#ekick
+@bot.command(name='kick')
+async def kick(ctx, user):
+  await kick(user)
 
 
 #Commands
