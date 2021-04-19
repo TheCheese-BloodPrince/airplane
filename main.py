845055460a4c266d6982411250887b0d7cee3dca
Aarav Karasi <karasiaarav@gmail.com>
2021-04-18 21:23:23 -0700

    Added base bot files.
    Includes main and keep_alive, should work right out of the box, although not recomended to run on your machine as it will create a web server there.

---
+++ main.py
@@ -0,0 +1,21 @@
+#Setting Up the Bot
+import os
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import json
+from keep_alive import keep_alive
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = commands.Bot(command_prefix=';')
+
+@bot.event
+async def on_ready():
+  print("nyooom has started up dun dun dun")
+
+
+#Commands
+#Running the Bot
+keep_alive()
+bot.run(TOKEN)
