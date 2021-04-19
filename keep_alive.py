50e2e1c0780163bba15cf6d502727f1a73867bbf
Aarav Karasi <karasiaarav@gmail.com>
2021-04-19 06:10:44 -0700

    Updated the values in the web server
    Updated the values in the web server to make them specific to airplane.

--- keep_alive.py
+++ keep_alive.py
@@ -3,7 +3,7 @@ from threading import Thread
 app = Flask('')
 @app.route('/')
 def home():
-  return "This is the web server for the nyooom bot."
+  return "This is the web server for the airplane bot. airplane is a Discord moderation bot with a build in economy."
 def run():
   app.run(host='0.0.0.0',port=8080)
 def keep_alive():
